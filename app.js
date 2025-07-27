5296ba1625d1bc2c7fa86463184b20a0b2fe4a77
Varun Verma <varun.verma2024@gmail.com>
2025-07-27 23:37:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import express from "express";
+
 import { PORT } from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
 const app = express();
 
@@ -7,6 +11,12 @@ app.get("/", (req, res) => {
     res.send("Hello World. This is Subscription Management");
 });
 
+// routes
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subs", subscriptionRouter);
+
+// Server Listening
 app.listen(PORT, () => {
     console.log(`Subscription Tracker API is running: http:localhost:${PORT}`);
 });
