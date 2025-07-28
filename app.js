85de50af2a22f8eeb645f365d90444cf26a2a4b0
Varun Verma <varun.verma2024@gmail.com>
2025-07-28 17:55:09 +0530

    Update app to use middlewares

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connect2database from "./database/connection.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Hello World. This is Subscription Management");
 });
+
+// Middleware
+app.use(express.json()); // allows api to handle json data sent
+app.use(express.urlencoded({ extended: false })); // allows api to handle url encoded data (from html forms) sent
+app.use(cookieParser()); // reads cookies from incoming requests so app can store user data
+app.use(errorMiddleware);
 
 // routes
 app.use("/api/v1/auth", authRouter);
