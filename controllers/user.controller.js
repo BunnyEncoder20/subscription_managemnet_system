049e282633b95705e4224ee747f7e29861fbaf89
Varun Verma <varun.verma2024@gmail.com>
2025-07-28 22:08:34 +0530

    Add controllers to get user(s)

---
+++ controllers/user.controller.js
@@ -0,0 +1,40 @@
+import UserModel from "../models/users.model.js";
+
+export const getAllUsers = async (req, res, next) => {
+    console.log("[server] fetching all users...");
+    try {
+        const users = await UserModel.find();
+
+        console.log("[sevrer] done");
+        res.status(200).json({
+            success: true,
+            data: users,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getUserById = async (req, res, next) => {
+    console.log(`[sever] fetching user:${req.param.id}`);
+    try {
+        // fetch the user by id (get all feilds - password cause we don't want to return that)
+        const user = await UserModel.findById(req.params.id).select(
+            "-password",
+        );
+
+        if (!user) {
+            const error = new Error("user not found");
+            error.statusCode = 404;
+            throw error;
+        }
+
+        console.log("[server] done");
+        res.status(200).json({
+            success: true,
+            data: user,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
