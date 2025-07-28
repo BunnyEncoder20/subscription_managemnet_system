f08bd69f3f61777bc9807cdedc160217884015d4
Varun Verma <varun.verma2024@gmail.com>
2025-07-28 23:21:01 +0530

    created arcject middlware

---
+++ middlewares/arcjet.middleware.js
@@ -0,0 +1,41 @@
+import aj from "../config/arcjet.js";
+
+const arcjetMiddleware = async (req, res, next) => {
+    console.log("[server] going through arcjet check");
+    try {
+        console.log("[server] arcjet making decision...");
+        const decision = await aj.protect(req);
+
+        if (decision.isDenied()) {
+            if (decision.reason.isRateLimit()) {
+                console.debug(
+                    "[server] arcjet rejected the request | rate limit exceeded",
+                );
+                return res.status(429).json({
+                    error: "rate limit exceeded",
+                });
+            }
+
+            if (decision.reason.isBot()) {
+                console.debug(
+                    "[server] arcjet rejected the request | bot detected",
+                );
+                return res.status(403).json({
+                    error: "bot detected",
+                });
+            }
+
+            console.debug(
+                "[server] arcjet rejected the request | access denied",
+            );
+            return res.status(403).json({ error: "access denied" });
+        }
+
+        next();
+    } catch (error) {
+        console.debug("[server] something went wrong in arcjet middleware");
+        next(error);
+    }
+};
+
+export default arcjetMiddleware;
