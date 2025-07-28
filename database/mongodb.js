e111f82355a3419f8382d79c0df46e5fa7cb5442
Varun Verma <varun.verma2024@gmail.com>
2025-07-28 14:51:01 +0530

    Update mongodb.js

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,19 +1,21 @@
-import mongoose from 'mongoose;
-import { DB_URI, NODE_ENV } from '../config/env';
+import mongoose from "mongoose";
+import { DB_URI, NODE_ENV } from "../config/env.js";
 
 if (!DB_URI) {
-    throw new Error("Please define the MongoDB connection URI env variable inside '.env<development/production>.local'")
+    throw new Error(
+        "Please define the MongoDB connection URI env variable inside '.env<development/production>.local'",
+    );
 }
 
 // connecting to Dat
 const connect2database = async () => {
     try {
         await mongoose.connect(DB_URI);
-        console.log(`Connected to database in ${NODE_ENV}`);
+        console.log(`Connected to database in ${NODE_ENV} mode`);
     } catch (error) {
         console.error("Error connecting to the database: ", error);
         process.exit(1);
     }
-}
+};
 
 export default connect2database;
