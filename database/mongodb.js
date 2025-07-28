ece566281152eb5e0fa3d36b66c1b55ad41594b0
Varun Verma <varun.verma2024@gmail.com>
2025-07-28 14:30:40 +0530

    Create Mongodb connection file

---
+++ database/mongodb.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose;
+import { DB_URI, NODE_ENV } from '../config/env';
+
+if (!DB_URI) {
+    throw new Error("Please define the MongoDB connection URI env variable inside '.env<development/production>.local'")
+}
+
+// connecting to Dat
+const connect2database = async () => {
+    try {
+        await mongoose.connect(DB_URI);
+        console.log(`Connected to database in ${NODE_ENV}`);
+    } catch (error) {
+        console.error("Error connecting to the database: ", error);
+        process.exit(1);
+    }
+}
+
+export default connect2database;
