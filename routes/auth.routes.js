365d233834204c72c40c1eb346b84c10b4306e20
Varun Verma <varun.verma2024@gmail.com>
2025-07-28 18:29:32 +0530

    Updated routes files to use controllers

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,24 +1,13 @@
 import { Router } from "express";
 
+// importing the controllers
+import { signUp, signIn, signOut } from "../controllers/auth.controller.js";
+
 // endpoints prefix: /api/v1/auth
 const authRouter = Router();
 
-authRouter.post("/sign-up", (req, res) => {
-    res.send({
-        msg: "Sign Up",
-    });
-});
-
-authRouter.post("/sign-in", (req, res) => {
-    res.send({
-        msg: "Sign In",
-    });
-});
-
-authRouter.post("/sign-out", (req, res) => {
-    res.send({
-        msg: "Sign Out",
-    });
-});
+authRouter.post("/sign-up", signUp);
+authRouter.post("/sign-in", signIn);
+authRouter.post("/sign-out", signOut);
 
 export default authRouter;
