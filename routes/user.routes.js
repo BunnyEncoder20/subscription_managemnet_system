a4e5877e740bd2b730fb731e33c21df77d9836ec
Varun Verma <varun.verma2024@gmail.com>
2025-08-01 11:41:00 +0530

    Added all pending controllers in user.routes file

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { getAllUsers, getUserById } from "../controllers/user.controller.js";
+import {
+    getAllUsers,
+    getUserById,
+    updateSpecificUser,
+    deleteUserById,
+} from "../controllers/user.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 // endpoint's prefix: api/v1/users
@@ -9,13 +14,10 @@ userRouter.get("/", authMiddleware, getAllUsers);
 userRouter.get("/:id", authMiddleware, getUserById); // need authorization route
 
 // TODO: Complete this route's controller
-userRouter.put("/:id", (req, res) => {
-    res.send({ message: "UPDATE specific user " });
-});
+userRouter.put("/:id", authMiddleware, updateSpecificUser);
 
 // TODO: Complete this route's controller
-userRouter.delete("/:id", (req, res) => {
-    res.send({ message: "DELETE specific user" });
-});
+userRouter.delete("/:id", authMiddleware, deleteUserById);
+p;
 
 export default userRouter;
